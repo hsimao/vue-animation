52da41e8c6e3759822454f3a1f068caa19cf98da
hsimao <e087754958@gmail.com>
2020-03-26 21:46:03 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <button @click="toggleModal">Open</button>
+    <transition name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leave-cancelled="leaveCancelled">
+      <div v-if="isOpen" class="modal">
+        <button @click="toggleModal">Close</button>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.isOpen = !this.isOpen
+    },
+    beforeEnter() {
+      console.log('beforeEnter')
+    },
+    enter() {
+      console.log('enter')
+    },
+    afterEnter() {
+      console.log('afterEnter')
+    },
+    enterCancelled() {
+      console.log('enterCancelled')
+    },
+    beforeLeave() {
+      console.log('beforeLeave')
+    },
+    leave() {
+      console.log('leave')
+    },
+    afterLeave() {
+      console.log('afterLeave')
+    },
+    leaveCancelled() {
+      console.log('leaveCancelled')
+    }
+  }
+}
+</script>
